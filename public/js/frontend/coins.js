3b86001d62c2c1a37096922c03bb686a9e77f46d
Webhero9297 <webhero9297@mail.com>
2018-04-19 02:46:07 +0800

    Webhero9297

--- public/js/frontend/coins.js
+++ public/js/frontend/coins.js
@@ -22,15 +22,8 @@ $(document).ready(function(){
     });
 
     loadLiveChart();
-    //window.setTimeout(function(){
-    //    window.setInterval(function(){
-    //        loadLiveChart();
-    //    }, 10000);
-    //}, 5000);
 
     $('#tfoot').css('display', 'none');
-
-    //$('#currency').change(doOnchangeCurrency);
 });
 function doOnLoadLiveChart() {
     $('#tfoot').css('display', 'none');
@@ -38,7 +31,6 @@ function doOnLoadLiveChart() {
     loadLiveChart();
 }
 function loadLiveChart() {
-    //$('#tbody_coin_live_data').html('<tr><td colspan="5" align="center"><div class="loader"></div></td></tr>');
     $.get('/coinpage/'+pos, {currency : selected_currency }, function(coin_live_datas){
         tbodyHTML = '';
         for( i=0;i<coin_live_datas.length; i++ ) {
