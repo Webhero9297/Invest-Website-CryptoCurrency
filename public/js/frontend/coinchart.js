6bdd0c98246a1465d0bfaf069983859327ea3286
Webhero9297 <webhero9297@mail.com>
2018-04-26 01:45:34 +0800

    webhero9297

---
+++ public/js/frontend/coinchart.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    window.setInterval(function(){
+        $.getJSON('/getcoinlivechartdata/'+coin_id, function(resp){
+            var trHTML = '';
+            var pString = '';
+            ( resp.percent_change_24h > 0) ? pString = "color-green" : pString = "color-red";
+            ( resp.percent_change_1h > 0) ? qString = "color-green" : qString = "color-red";
+            trHTML = '<tr>\
+                                <td class="td-cell">$'+resp.price_usd+'</td>\
+                                <td class="td-cell '+pString+'">\
+                                '+resp.percent_change_24h+'%\
+                                </td>\
+                                <td class="td-cell '+qString+'">\
+                                '+resp.percent_change_1h+'%\
+                                </td>\
+                                <td class="td-cell">'+resp.mkt_cap_usd+'</td>\
+                            <td class="td-cell">'+resp.h24_vol_usd+'</td>\
+                        </tr>';
+            $('#tbody_content').html(trHTML);
+        });
+    }, 30000);
+});
