87c84ceda599dafa584f7fac800a45835be754fe
Webhero9297 <webhero9297@mail.com>
2018-05-05 02:55:51 +0800

    webhero9297

--- resources/views/frontend/portfolio.blade.php
+++ resources/views/frontend/portfolio.blade.php
@@ -52,7 +52,6 @@
             border-color: gold;
         }
     </style>
-    {{--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">--}}
     <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css">
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" ></script>
     <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js" ></script>
