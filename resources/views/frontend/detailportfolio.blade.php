c3094b3161e607d2ad2b8dced53d2f95d6faaeb4
Webhero9297 <webhero9297@mail.com>
2018-05-08 22:31:31 +0800

    webhero9297

--- resources/views/frontend/detailportfolio.blade.php
+++ resources/views/frontend/detailportfolio.blade.php
@@ -224,7 +224,6 @@
     </div>
 </div>
 
-{{--<script src="{{ asset('./js/frontend/editprofile.js') }}"></script>--}}
 <div id="fb-root"></div>
 <script>
     var fbButton = document.getElementById('fb-share-button');
